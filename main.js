ee8cad4ce0d9591dbda878ffa048dba6728820cc
Baekshook <ecgo0106@naver.com>
2022-09-21 20:42:23 +0900

    add animation1

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-function revealMyInfo(myInfo, className, color) {
+function revealMyInfo(myInfo, className, color, animationName) {
   let htmlElement = document.querySelector(className);
 
   htmlElement.innerHTML = myInfo;
   htmlElement.style.color = color;
+
+  htmlElement.style.animationDuration = "2s";
+  if (animationName === "bgFadeOut") {
+    htmlElement.style.animationName = animationName;
+  } else if (animationName === "slideIn") {
+    htmlElement.style.animationName = animationName;
+  } else if (animationName === "heartBeat") {
+    htmlElement.style.animationName = animationName;
+  }
 }
