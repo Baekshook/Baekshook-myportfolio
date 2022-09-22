4cb5ccfcacf8cf86cd9541d51282f6972bc53886
Baekshook <ecgo0106@naver.com>
2022-09-22 21:44:22 +0900

    220922 class

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+const todayLunch = [
+  "알리오 올리오",
+  "치킨",
+  "피자",
+  "짜장면",
+  "굶기",
+  "햄버거",
+  "떡볶이",
+  "짬뽕",
+  "도넛",
+  "사케동",
+  "텐동",
+  "국밥",
+  "탕수육",
+  "회",
+];
+
+const myProfile = {
+  age: 24,
+  job: "University Student",
+  favorite: "Zero Pepsi",
+  motto: "Carpe Diem",
+};
+
 function revealMyInfo(myInfo, className, color, animationName) {
   let htmlElement = document.querySelector(className);
 
@@ -12,4 +36,17 @@ function revealMyInfo(myInfo, className, color, animationName) {
   } else if (animationName === "heartBeat") {
     htmlElement.style.animationName = animationName;
   }
+}
+
+function choiceTodayLunch() {
+  let htmlElement = document.querySelector(".todayLunch");
+
+  htmlElement.innerHTML =
+    todayLunch[Math.floor(Math.random() * todayLunch.length)];
+}
+
+function getMyProfile(key) {
+  let htmlElement = document.querySelector(".aboutMe");
+
+  htmlElement.innerHTML = myProfile[key];
 }
